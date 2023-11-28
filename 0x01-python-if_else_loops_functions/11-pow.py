8bd176da688f41e95850fd4e2ceb6bba97ae4c79
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-11-28 09:59:23 -0800

    11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """Computes a to the power of b and returns the value."""
+    return a ** b
+
+
+if __name__ == "__main__":
+    pow = __import__('11-pow').pow
+
+    print(pow(2, 2))
+    print(pow(98, 2))
+    print(pow(98, 0))
+    print(pow(100, -2))
+    print(pow(-4, 5))
