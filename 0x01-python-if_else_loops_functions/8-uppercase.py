d6ba4170ebca54e37916018c5d611140ca712a47
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-11-28 09:18:25 -0800

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,14 +3,14 @@
 def uppercase(s):
     """Prints a string in uppercase followed by a new line."""
     for char in s:
-        upper_char = char
         if ord('a') <= ord(char) <= ord('z'):
             upper_char = chr(ord(char) - ord('a') + ord('A'))
+        else:
+            upper_char = char
         print("{}".format(upper_char), end='')
 
     print()  # Add a newline at the end
 
-    
 if __name__ == "__main__":
     uppercase = __import__('8-uppercase').uppercase
 
