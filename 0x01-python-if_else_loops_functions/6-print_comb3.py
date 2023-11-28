148530af721563881711a39723ecd3f0ab837527
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-11-28 09:07:53 -0800

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i != j:
-            if i < 9:
+            if i < 8:  # Update to 8 to avoid printing comma after the last combination
                 print("{:d}{:d}, ".format(i, j), end='')
             else:
                 print("{:d}{:d}".format(i, j))
