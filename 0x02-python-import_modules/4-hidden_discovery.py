850e06a7b6d77ffd85fb63f200583002d06be3c1
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-11-30 10:19:58 -0800

    4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.8
+import dis
+import hidden_4
+
+if __name__ == "__main__":
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+
+    for name in sorted(names):
+        print(name)
