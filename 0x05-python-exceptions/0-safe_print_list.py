a3880f08da88351dc239e60ccaa65f8ae4a07ec6
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-12-19 10:56:26 -0800

    mandatory

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,15 +11,3 @@ def safe_print_list(my_list=[], x=0):
     finally:
         print()
         return printed_elements
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-
-    nb_print = safe_print_list(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-
-    nb_print = safe_print_list(my_list, len(my_list))
-    print("nb_print: {:d}".format(nb_print))
-
-    nb_print = safe_print_list(my_list, len(my_list) + 2)
-    print("nb_print: {:d}".format(nb_print))
